fd712d2553faebf3a630a8229a888ba37086ee89
Andrewki44 <andrewkilbourn1996@gmail.com>
2026-04-20 00:34:47 +1000

    Add project files.

---
+++ main.cs
@@ -0,0 +1,14 @@
+﻿
+using REFrameworkNET;
+using REFrameworkNET.Attributes;
+
+namespace PRAGMATA_AP;
+
+public class MyPlugin
+{
+    [PluginEntryPoint]
+    public static void Main()
+    {
+        API.LogInfo("Hello World");     
+    }
+}
