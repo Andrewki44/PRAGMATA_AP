2e8ba04cc2a872f51442e905d323ead8d0966a8c
Andrewki44 <andrewkilbourn1996@gmail.com>
2026-04-26 02:46:28 +1000

    Archipelago Connectivity

---
+++ src/Archipelago/Archipelago.cs
@@ -0,0 +1,153 @@
+﻿using Archipelago.MultiClient.Net;
+using Archipelago.MultiClient.Net.Enums;
+using Archipelago.MultiClient.Net.Helpers;
+using Archipelago.MultiClient.Net.MessageLog.Messages;
+using Archipelago.MultiClient.Net.MessageLog.Parts;
+using Archipelago.MultiClient.Net.Models;
+using Archipelago.MultiClient.Net.Packets;
+using REFrameworkNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PRAGMATA_AP;
+
+public static partial class Archipelago {
+    public  static Lock                clientLock = new();
+    public  static ArchipelagoSession? currentSession;
+    public  static string?             currentServer;
+    public  static bool                local_locations_updated = false;
+    public  static bool                remote_locations_updated = false;
+    public  static string?             seedID = null;
+    private static bool                _isDisconnecting = false;
+
+    public static PlayerInfo? activePlayer => currentSession?.Players.ActivePlayer;
+    public static bool isConnected => currentSession != null;
+
+    public static async Task Connect(string server, string user, string password) {
+        API.LogInfo($"~~ AP Connect ~~");
+        LoginResult loginResult;
+        ArchipelagoSession? session = null;
+        if (_isDisconnecting) return;
+
+        // Create new session and connect asynchronously
+        try {
+            session = ArchipelagoSessionFactory.CreateSession(server);
+            connectHandlers(session);
+            RoomInfoPacket infoPacket = await session.ConnectAsync();
+
+            loginResult = await session.LoginAsync("Pragmata", user, ItemsHandlingFlags.RemoteItems, Version.Parse("0.6.0"), password: password, requestSlotData: true);
+        } catch (Exception e) {
+            loginResult = new LoginFailure(e.GetBaseException().Message);
+        }
+
+        // If connection succeeded
+        if (loginResult.Successful) {
+            LoginSuccessful loginSuccess = (LoginSuccessful)loginResult;
+            //seedID = loginSuccess.SlotData["SeedID"].ToString();
+            currentServer = server;
+            currentSession = session;
+            API.LogInfo($"~~ Successfully connected to {server} as {user}");
+            return;
+        } else {    // Connection failed
+            LoginFailure loginFail = (LoginFailure)loginResult;
+            string errorMessage = $"~~ Failed to connect to {server} as {user}:";
+
+            foreach (string error in loginFail.Errors)
+                errorMessage += $"\n    {error}";
+
+            foreach (ConnectionRefusedError error in loginFail.ErrorCodes)
+                errorMessage += $"\n    {error}";
+
+            currentSession = null;
+            API.LogError(errorMessage);
+            return;
+        }
+    }
+
+    public static async Task Disconnect(ArchipelagoSession? session = null) {
+        session ??= currentSession;
+        if (session is null || _isDisconnecting)
+            return;
+
+        _isDisconnecting = true;
+        await session.Socket.DisconnectAsync();
+        API.LogWarning("~~ AP Disconnected ~~");
+        //}
+    }
+
+    private static void connectHandlers(ArchipelagoSession session) {
+#if DEBUG
+        API.LogInfo("~~ Connect Handlers ~~");
+#endif
+        session.MessageLog.OnMessageReceived += MessageLog_OnMessageReceived;
+        session.Socket.ErrorReceived += Socket_ErrorReceived;
+        session.Socket.SocketOpened += Socket_SocketOpened;
+        session.Socket.SocketClosed += Socket_SocketClosed;
+        session.Locations.CheckedLocationsUpdated += Locations_CheckedLocationsUpdated;
+    }
+
+    private static void Locations_CheckedLocationsUpdated(System.Collections.ObjectModel.ReadOnlyCollection<long> newCheckedLocations) {
+        lock (clientLock) {
+            remote_locations_updated = true;
+        }
+    }
+
+    private static void Socket_ErrorReceived(Exception e, string message) {
+        API.LogError($"~~ Socket Error: {message} ~~");
+        API.LogError($"~~ Socket Exception: {e.Message} ~~");
+
+        if (e.StackTrace != null)
+            foreach (var line in e.StackTrace.Split('\n'))
+                API.LogError($"    {line}");
+        else
+            API.LogError($"    No Stacktrace Provided");
+
+        Socket_SocketClosed("");
+    }
+
+    private static void Socket_SocketOpened() {
+#if DEBUG
+        API.LogInfo($"Socket Opened: \"{currentSession?.Socket.Uri}\"");
+#endif
+    }
+
+    private static void Socket_SocketClosed(string reason) {
+#if DEBUG
+        API.LogWarning($"Socket Closed: \"{reason}\"");
+#endif
+        Console.AddLogMessage([($"Disconnected from server ({reason})", Color.Red)]);
+        lock (clientLock) {
+            currentSession = null;
+            seedID = null;
+            currentServer = null;
+            _isDisconnecting = false;
+        }
+    }
+
+    private static void MessageLog_OnMessageReceived(LogMessage message) {
+        MessagePart[] parts = message.Parts;
+        List<(string, Color)> messageParts = parts.Select((part) => {
+            Color color = part.Color;
+            if (part.IsBackgroundColor) {
+                color = Color.White;
+            }
+            else if (part.Color == Color.Black) {
+                color = new(128, 128, 128);
+            }
+            return (part.Text, color);
+        }).ToList();
+        Console.AddLogMessage(messageParts);
+    }
+
+    private static void SayAsync(string message) {
+        lock (clientLock) {
+            if (isConnected) {
+                currentSession!.Socket.SendPacketAsync(new SayPacket { Text = message });
+            }
+        }
+    }
+}
+
---
+++ src/Archipelago/Console.cs
@@ -0,0 +1,195 @@
+﻿using Hexa.NET.ImGui;
+using REFrameworkNET;
+using REFrameworkNET.Attributes;
+using REFrameworkNET.Callbacks;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading;
+using Color = Archipelago.MultiClient.Net.Models.Color;
+
+namespace PRAGMATA_AP;
+
+public static partial class Archipelago {
+    public static class Console {
+        private const  ImGuiKey _consoleKey = ImGuiKey.F8;
+        private static bool     _enabled    = false;
+
+#if DEBUG
+        private const  string _defaultClientAddress = "localhost:38281";
+#else
+        private const  string _defaultClientAddress = "archipelago.gg:";
+#endif
+
+        private static string clientAddress         = _defaultClientAddress;
+        private static string clientName            = "";
+        private static string clientPassword        = "";
+
+        private static readonly Lock                          clientLogLock = new();
+        private static List<List<(string text, Color color)>> clientLog = [];
+        private static bool                                   clientLogUpdated = false;
+
+        private static string                        clientInput = "";
+        private static LinkedList<string>            clientInputHistory = new();
+        private static LinkedListNode<string>?       clientInputHistoryCurrent = null;
+        private static unsafe ImGuiInputTextCallback clientInputImGuiInputTextCallback = ClientInputImGuiInputTextCallback;
+        private static bool                          focusClientInput = false;
+
+        private static int fontSize = -1;
+
+        [Callback(typeof(ImGuiRender), CallbackType.Pre)]
+        public static void OnImGuiRender() {
+            if (fontSize == -1)
+                fontSize = (int)ImGui.GetFontSize();
+
+            ImGui.PushFont(null, fontSize);
+            //ImGui.SetNextWindowPos(ImGui.GetCenter(ImGui.GetMainViewport()), ImGuiCond.Appearing, new(0.5f, 0.5f));
+
+            RenderConsole();
+
+            ImGui.PopFont();
+        }
+
+        private static void RenderConsole() {
+            _enabled ^= ImGui.IsKeyPressed(_consoleKey);
+            if (!_enabled)
+                return;
+
+            ImGuiStylePtr style = ImGui.GetStyle();
+
+            if (ImGui.Begin("Archipelago###Archipelago.Console")) {
+                if (ImGui.BeginTabBar("TabBar###Archipelago.Console.TabBar")) {
+                    if (ImGui.BeginTabItem("Main###Archipelago.Console.TabBar.Main")) {
+                        RenderConnection();
+                        RenderLog();
+
+                        ImGui.EndTabItem();
+                    }
+                    ImGui.EndTabBar();
+                }
+            }
+
+            ImGui.End();
+        }
+
+        private static void RenderConnection() {
+            if (!isConnected) {
+                ImGui.InputText("Address", ref clientAddress, 50);
+                ImGui.InputText("Name", ref clientName, 50);
+                ImGui.InputText("Password", ref clientPassword, 50);
+
+                if (ImGui.Button("Connect")) {
+                    //_ = Connect(clientAddress, clientName, clientPassword);
+                    Connect(clientAddress, clientName, clientPassword).Wait();
+                }
+                if (ImGui.Button("Disconnect")) {
+                    Disconnect().Wait();
+                }
+            } else {
+                ImGui.Text($"Connected as {activePlayer?.Name}");
+                if (ImGui.Button("Disconnect")) {
+                    Disconnect().Wait();
+                }
+            }
+        }
+
+        private static void RenderLog() {
+            ImGuiStylePtr style = ImGui.GetStyle();
+            if (ImGui.BeginChild("Archipelago.Console.Log", new(0, ImGui.GetContentRegionAvail().Y - ImGui.GetTextLineHeight() - 3 * style.ItemSpacing.Y), ImGuiChildFlags.Borders, ImGuiWindowFlags.NoMove)) {
+                //var curr_scroll = ImGui.GetScrollY() / previous_scroll_max;
+                lock (clientLogLock) {
+                    foreach (var line in clientLog) {
+                        byte part_counter = 0;
+                        int part_length = line.Count;
+                        //ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, new Vector2(0,style.ItemSpacing.Y));
+                        //ImGui.PushTextWrapPos(ImGui.GetCursorPosX() + ImGui.GetWindowWidth() - style.WindowPadding.X);
+                        var wrap_width = ImGui.GetContentRegionAvail().X;
+                        var remaining_width = wrap_width;
+                        foreach (var part in line) {
+                            var color = new Vector4(part.color.R / 255f, part.color.G / 255f, part.color.B / 255f, 1.0f);
+                            ImGui.PushStyleColor(ImGuiCol.Text, color);
+                            foreach (var word in part.text.Split(" ")) {
+                                var word_width = ImGui.CalcTextSize($"{word} ").X;
+                                if (part_counter > 0 && word_width < remaining_width) {
+                                    ImGui.SameLine();
+                                    ImGui.TextUnformatted(word);
+                                    remaining_width -= word_width;
+                                } else {
+                                    ImGui.TextUnformatted(word);
+                                    remaining_width = wrap_width - word_width;
+                                }
+                                part_counter++;
+                            }
+                            ImGui.PopStyleColor();
+                            //ImGui.TextWrapped(line);
+                        }
+                        //ImGui.PopTextWrapPos();
+                        //ImGui.PopStyleVar();
+                    }
+                }
+                if (clientLogUpdated) {
+                    ImGui.SetScrollHereY();
+                    clientLogUpdated = false;
+                }
+            }
+            ImGui.EndChild();
+
+            if (focusClientInput) {
+                ImGui.SetKeyboardFocusHere();
+                focusClientInput = false;
+            }
+
+            bool process_input = ImGui.InputText("Input", ref clientInput, 150,
+                ImGuiInputTextFlags.EnterReturnsTrue | ImGuiInputTextFlags.CallbackHistory,
+                clientInputImGuiInputTextCallback);
+
+            if (process_input && clientInput.Length > 0) {
+                clientInputHistory.AddFirst(clientInput);
+                if (clientInputHistory.Count > 10) {
+                    clientInputHistory.RemoveLast();
+                }
+                clientInputHistoryCurrent = null;
+
+                focusClientInput = true;
+
+                if (!clientInput.StartsWith("/")) {
+                    // Say
+                    SayAsync(clientInput);
+                } else {
+                    // Client command
+                }
+                clientInput = "";
+            }
+        }
+
+        public static void AddLogMessage(List<(string, Color)> message) {
+            lock (clientLogLock) {
+                clientLog.Add(message);
+                clientLogUpdated = true;
+            }
+        }
+
+        private unsafe static int ClientInputImGuiInputTextCallback(ImGuiInputTextCallbackData* data) {
+            if (data->EventFlag == ImGuiInputTextFlags.CallbackHistory) {
+                if (data->EventKey == ImGuiKey.UpArrow) {
+                    if (clientInputHistoryCurrent is null && clientInputHistory.First is not null) {
+                        clientInputHistoryCurrent = clientInputHistory.First;
+                        data->DeleteChars(0, data->BufTextLen);
+                        data->InsertChars(0, clientInputHistoryCurrent.Value);
+                    } else if (clientInputHistoryCurrent?.Next is not null) {
+                        clientInputHistoryCurrent = clientInputHistoryCurrent.Next;
+                        data->DeleteChars(0, data->BufTextLen);
+                        data->InsertChars(0, clientInputHistoryCurrent.Value);
+                    }
+                } else if (data->EventKey == ImGuiKey.DownArrow) {
+
+                    clientInputHistoryCurrent = clientInputHistoryCurrent?.Previous;
+                    data->DeleteChars(0, data->BufTextLen);
+                    if (clientInputHistoryCurrent is not null) {
+                        data->InsertChars(0, clientInputHistoryCurrent.Value);
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
